73d1d785f5debf1bc90c93c1a9be28b607dd1375
Sean Chavez <realseanchavez@gmail.com>
2019-01-09 15:28:51 -0800

    Implement getLevelBBData

--- LevelSandbox.js
+++ LevelSandbox.js
@@ -5,38 +5,40 @@
 const level = require('level');
 const chainDB = './chaindata';
 
-
 class LevelSandbox {
+  constructor() {
+    this.db = level(chainDB);
+  }
 
-    constructor() {
-        this.db = level(chainDB);
-    }
+  // Get data from levelDB with key (Promise)
+  getLevelDBData(key) {
+    let self = this;
+    return new Promise(function(resolve, reject) {
+      self.db.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
 
-    // Get data from levelDB with key (Promise)
-    getLevelDBData(key){
-        let self = this;
-        return new Promise(function(resolve, reject) {
-            // Add your code here, remember un Promises you need to resolve() or reject()
-        });
-    }
+  // Add data to levelDB with key and value (Promise)
+  addLevelDBData(key, value) {
+    let self = this;
+    return new Promise(function(resolve, reject) {
+      // Add your code here, remember un Promises you need to resolve() or reject()
+    });
+  }
 
-    // Add data to levelDB with key and value (Promise)
-    addLevelDBData(key, value) {
-        let self = this;
-        return new Promise(function(resolve, reject) {
-            // Add your code here, remember un Promises you need to resolve() or reject() 
-        });
-    }
-
-    // Method that return the height
-    getBlocksCount() {
-        let self = this;
-        return new Promise(function(resolve, reject){
-            // Add your code here, remember un Promises you need to resolve() or reject()
-        });
-    }
-        
-
+  // Method that return the height
+  getBlocksCount() {
+    let self = this;
+    return new Promise(function(resolve, reject) {
+      // Add your code here, remember un Promises you need to resolve() or reject()
+    });
+  }
 }
 
 module.exports.LevelSandbox = LevelSandbox;
