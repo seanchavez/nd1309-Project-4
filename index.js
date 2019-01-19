acce4895d37ed82e75b3f9a6dffbefa535f715d8
Sean Chavez <realseanchavez@gmail.com>
2019-01-19 00:31:36 -0800

    Implement POST new block endpoint

--- index.js
+++ index.js
@@ -21,4 +21,18 @@ app.get('/block/:height', async (req, res) => {
   }
 });
 
+app.post('/block', async (req, res) => {
+  try {
+    const block = await bc.addBlock(req.body);
+    console.log('Block: ', block);
+    if (block) {
+      res.status(201).json(block);
+    } else {
+      res.status(400).json({ error: 'Block could not be added' });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
 app.listen(port, () => console.log(`Running on ${port}`));
